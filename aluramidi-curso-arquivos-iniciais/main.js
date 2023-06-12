2a06c3c545e4333bdf05453722214ecd67318b90
Kelvin Perez <kelvin_klncs@hotmail.com>
2023-06-11 21:40:57 -0300

    ultimas insercoes no projeto

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,18 +1,23 @@
 function playSound(elementoAudio){
-    document.querySelector(elementoAudio).play();
-    
-}
+    elemento = document.querySelector(elementoAudio);
+    if (elemento != null && elemento.localName === 'audio'){
+            elemento.play();
+    }
+    else {
+        console.log('elemento nao localizado')
+    }
+     }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0
 
-while (contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
     
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const aud = '#som_' + instrumento;
-    //console.log(instrumento);
+    console.log(contador);
    // console.log(aud);
 
 
@@ -21,7 +26,19 @@ while (contador < listaDeTeclas.length){
         
     }
     
-    contador = (contador + 1);
+    
+    tecla.onkeydown = function(event){
+        console.log(event.code)
+        if (event.code === 'Enter'  || event.code === 'Space') {
+        tecla.classList.add('ativa')
+        }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+        }
+    }    
+    
+    
    // console.log(contador)
 
 }
